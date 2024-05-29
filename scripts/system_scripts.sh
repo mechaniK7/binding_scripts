908d437de637a6ec134edbd6a117a80fec030a70
Nikita Chernyshev <nikitosss140604@gmail.com>
2024-05-29 15:01:23 +0300

    Added a part of the functions for determining the IP address

--- scripts/system_scripts.sh
+++ scripts/system_scripts.sh
@@ -8,3 +8,32 @@ function setsrc
     echo "---- Result:"
     echo "source $(pwd)/install/setup.bash"
 }
+
+# This script
+function showip
+{
+    echo "-------------------------------------------------------"
+    #ip address show $1
+
+    # Получить имя сетевого интерфейса
+    #interface=$(ip route get 8.8.8.8 | awk '{print $3}')
+    interface=$1
+
+    # Получить динамический IP-адрес
+    dynamic_ip=$(ip addr show $interface | grep 'inet\ .*dynamic' | awk '{print $2}')
+
+    # Проверить, установлен ли статический IP-адрес
+    static_ip=$(ip addr show $interface | grep 'inet\ .*brd' | awk '{print $2}')
+
+    # Если статический IP-адрес не установлен, вывести сообщение
+    if [ -z "$static_ip" ]; then
+    echo "Статический IP-адрес не установлен."
+    else
+    # Вывести статический IP-адрес
+    echo "Статический IP-адрес: $static_ip"
+    fi
+
+    # Вывести динамический IP-адрес
+    echo "Динамический IP-адрес: $dynamic_ip"
+
+}
