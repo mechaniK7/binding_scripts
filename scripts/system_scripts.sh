39608e7cd84544ee1efc2f9261b5840f8432e8a4
Nikita Chernyshev <nikitosss140604@gmail.com>
2024-06-04 17:33:57 +0300

    Showip function update Added definition of two IP addresses of the same type and written output of additional information about the network and network devices.

--- scripts/system_scripts.sh
+++ scripts/system_scripts.sh
@@ -13,29 +13,65 @@ function setsrc
 function showip
 {
     echo "-------------------------------------------------------"
-    #ip address show $1
 
-    # Получить имя сетевого интерфейса
-    #interface=$(ip route get 8.8.8.8 | awk '{print $3}')
-    interface=$1
+    # Получить имена сетевых устройств
+    wifi_device=$(nmcli device status | grep 'wifi ' | awk '{print $1}')
+    eth_device=$(nmcli device status | grep 'ethernet ' | awk '{print $1}')
 
-    # Получить динамический IP-адрес
-    dynamic_ip=$(ip addr show $interface | grep 'dynamic' | awk '{print $2}' | cut -f1 -d'/')
+    # Получить IP-адреса
+    dynamic_wifi_ip=$(ip addr show | grep 'dynamic' | grep $wifi_device | awk '{print $2}' | cut -f1 -d'/')
+    static_wifi_ip=$(ip addr show | grep -v 'dynamic' | grep 'inet\ .*noprefix' | grep $wifi_device | awk '{print $2}' | cut -f1 -d'/')
 
-    # Проверить, установлен ли статический IP-адрес
-    static_ip=$(ip addr show | grep -v "dynamic" | grep 'inet\ .*noprefix' | awk '{print $2}' | cut -f1 -d'/')
+    dynamic_eth_ip=$(ip addr show | grep 'dynamic' | grep $eth_device | awk '{print $2}' | cut -f1 -d'/')
+    static_eth_ip=$(ip addr show | grep -v 'dynamic' | grep 'inet\ .*noprefix' | grep $eth_device | awk '{print $2}' | cut -f1 -d'/')
 
-    if [ -z "$dynamic_ip" ]; then
-        echo "Динамический IP-адрес не установлен."
-    else
-        # Вывести динамический IP-адрес
-        echo "Динамический IP-адрес: $dynamic_ip"
-    fi
-
-    if [ -z "$static_ip" ]; then
+    if [ ! -z "$dynamic_wifi_ip" ] && [ ! -z "$dynamic_eth_ip" ]; then
+        echo "Динамический IP-адрес: $dynamic_wifi_ip | Тип подключения: Wifi | Устройство: $wifi_device"
+        echo "Динамический IP-адрес: $dynamic_eth_ip | Тип подключения: Ethernet | Устройство: $eth_device"
         echo "Статический IP-адрес не установлен."
     else
-        # Вывести статический IP-адрес
-        echo "Статический IP-адрес: $static_ip"
+        if [ ! -z "$static_wifi_ip" ] && [ ! -z "$static_eth_ip" ]; then
+            echo "Динамический IP-адрес не установлен."
+            echo "Статический IP-адрес: $static_wifi_ip | Тип подключения: Wifi | Устройство: $wifi_device"
+            echo "Статический IP-адрес: $static_eth_ip | Тип подключения: Ethernet | Устройство: $eth_device"
+        else
+            ####
+            # Получить IP-адреса
+            dynamic_ip=$(ip addr show | grep 'dynamic' | awk '{print $2}' | cut -f1 -d'/')
+            static_ip=$(ip addr show | grep -v 'dynamic' | grep 'inet\ .*noprefix' | awk '{print $2}' | cut -f1 -d'/')
+
+            if [ -z "$dynamic_ip" ]; then
+                echo "Динамический IP-адрес не установлен."
+            else
+                # Получить имя сетевого устройста в соответствии с полученным IP-адресом
+                get_dynamic_device=$(ip addr show | grep $dynamic_ip | awk '{print $9}')
+                
+                if [ "$wifi_device" = "$get_dynamic_device" ]; then
+                    net_info=Wifi
+                else
+                    net_info=Ethernet
+                fi
+
+                # Вывести динамический IP-адрес
+                echo "Динамический IP-адрес: $dynamic_ip | Тип подключения: $net_info | Устройство: $get_dynamic_device"
+            fi
+
+            if [ -z "$static_ip" ]; then
+                echo "Статический IP-адрес не установлен."
+            else
+                # Получить имя сетевого устройста в соответствии с полученным IP-адресом
+                get_static_device=$(ip addr show | grep $static_ip | awk '{print $8}')
+
+                if [ "$eth_device" = "$get_static_device" ]; then
+                    net_info=Ethernet
+                else
+                    net_info=Wifi
+                fi
+
+                # Вывести статический IP-адрес
+                echo "Статический IP-адрес: $static_ip | Тип подключения: $net_info | Устройство: $get_static_device"
+            fi
+            ####
+        fi
     fi
 }
